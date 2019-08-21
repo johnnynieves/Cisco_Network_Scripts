d53dc9a741aec9038aa17e59ebf5838c384c1868
Johnny Nieves <50960412+johnnynieves@users.noreply.github.com>
2019-08-21 11:22:15 -0700

    Test2
    Changed the logic of accessing the switch and the csv file. program now can identify switchports and compare with matrix data

--- rename_interfaces.py
+++ rename_interfaces.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import sys
 import os
 import json
@@ -6,28 +5,36 @@ import csv
 from pprint import pprint
 from napalm import get_network_driver
 
-#Open the matrix and set variables to indexes
-with open('test.csv','r') as matrix:
-    matrix_data =  csv.reader(matrix)
-    for line in matrix_data:
-        matrix_interface = line[0]
-        matrix_description = line[1]
-        matrix_mac = line[2]
-        print(matrix_interface,matrix_description,matrix_mac)
-        #open switch database
-        with open('devices.txt','r') as switch_db:
-            for switch in switch_db:
-                #set up to connect to a switch from switch_db
-                driver = get_network_driver('ios')
-                with driver(switch, 'jnieves', 'johnny') as device:
-                    print('Connecting to... ', switch)
-                    switch_int_data = device.get_interfaces()
-                    for interface in switch_int_data:
-                        print(switch_int_data[interface]['description'])
-                    #    print(switch)
-                    #switch.load_merge_candidate(filename='config.txt')
-                    #print(switch.compare_config())
 
+#open switch database
+with open('devices.txt','r') as switch_db:
+    for switch in switch_db:
+        #set up to connect to a switch from switch_db
+        driver = get_network_driver('ios')
+        with driver(switch, 'jnieves', 'johnny') as device:
+            print('Connecting to... ', switch)
+            switch_int_data = device.get_interfaces()
+            #Open the matrix and set variables to indexes
+            with open('test.csv','r') as matrix:
+                matrix_data =  csv.reader(matrix)
+                next(matrix_data)
+                for line in matrix_data:
+                        matrix_interface = line[0]
+                        matrix_description = line[1]
+                        matrix_mac = line[2]
+                        matrix_list = [matrix_interface,matrix_description,matrix_mac]
+                        print('Searching for ... ',matrix_list)
+                        for interface in switch_int_data:
+                            int = interface
+                            desc = switch_int_data[interface]['description']
+                            mac = switch_int_data[interface]['mac_address']
+                            if matrix_mac == mac:
+                                print('found it!')
+                                if desc != matrix_description:
+                                    print(str(int),"'s Needs to be changed")
 
-#            for switch in device
-#            with driver('192.168.122.250', 'jnieves', 'johnny') as devices:
+                            #config = 'CONF T \nINT ' + str(int) + '\nDESC ' + str(matrix_description) + '\nDO WR\n'
+                                #print(config)
+                                #lines = device.load_merge_candidate(config = 'CONF T \nINT ' + str(int) + '\nDESC ' + str(matrix_description) + $
+                                #print('Diff:')
+                                #print(device.compare_config())
