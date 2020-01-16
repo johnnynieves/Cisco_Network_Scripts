e7ed630e625aff96f7b6b66205031a14b60c6357
Johnny Nieves <50960412+johnnynieves@users.noreply.github.com>
2020-01-15 21:42:50 -0800

    Update Checking_Changing_portfast_state.py

--- Checking_Changing_portfast_state.py
+++ Checking_Changing_portfast_state.py
@@ -3,13 +3,43 @@ from napalm.base.exceptions import ConnectionException
 from getpass import getpass
 from netmiko import NetMikoAuthenticationException
 from pprint import pprint
-import csv
-from csv import DictReader, DictWriter
+import os
 
 driver = get_network_driver('ios')
 
 
 def main():
+    print('*' * 80)
+    print('*', ' ' * 30, ' Network Tools ', ' ' * 29, '*')
+    print('*' * 80)
+    print()
+    print()
+    print('*' * 80)
+    print()
+    print('Please Select a tool')
+    print()
+    print('1. List Enabled Ports')
+    print('2. Enable portfast on ports in list')
+    print('0. to quit')
+    print()
+    print('*' * 80)
+    print()
+    tool = int(input('Enter your selection '))
+
+    if tool == 1:
+        listx()
+        # clear screen
+        # if os.system('cls')
+
+    elif tool == 2:
+        enable_portfast()
+
+    else:
+        print('Good Bye')
+        exit()
+
+
+def enable_portfast():
     username = 'jnieves'
     password = 'johnny'
     devices = '10.231.27.'
@@ -19,21 +49,22 @@ def main():
             ip = devices + str(sw)
             device = driver(ip, username, password)
             device.open()
-            print(f'Connecting to {ip} ')
-            print('-' * 80)
+            print(f'\nConnecting to {ip}')
+            print('-' * 80 + '\n')
             data = device.get_interfaces()
             for i in data:
                 if i[0] == 'G' and data[i]['is_up'] == True and data[i]['is_enabled'] == True:
-                    print(f'{i} is up')
                     print(device.device.send_config_set(
                         ['interface ' + i, 'spanning-tree portfast edge', 'end']))
             print(device.device.send_command('wr'))
 
         except NetMikoAuthenticationException:
             print('Auth Error for ', ip)
+
         except ConnectionException:
             print('Could not connect to ', ip)
     device.close()
+    main()
 
 
 def listx():
@@ -46,21 +77,23 @@ def listx():
             ip = devices + str(sw)
             device = driver(ip, username, password)
             device.open()
-            print(f'Connecting to {ip} ')
-            print('-' * 80)
+            print(f'\nConnecting to {ip}')
+            print('-' * 80 + '\n')
             data = device.get_interfaces()
-            # Here open file
-            # make in file description title
-            # what switch(IP)
+            f = open(f'{ip}_access_ports_up.txt', 'w')
+            f.write('Switch' + ', ' + ip + '\n\n')
             for i in data:
                 if i[0] == 'G' and data[i]['is_up'] == True and data[i]['is_enabled'] == True:
-                    # write line to file "interface(i),"
-
+                    f.write(
+                        f'{i}, ' + f"{data[i]['description']}, " + 'is_up\n')
+            print('Making File...')
+            f.close()
         except NetMikoAuthenticationException:
             print('Auth Error for ', ip)
         except ConnectionException:
             print('Could not connect to ', ip)
     device.close()
+    main()
 
 
 if __name__ == "__main__":
